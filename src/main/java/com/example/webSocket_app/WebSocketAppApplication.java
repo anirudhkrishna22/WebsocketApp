1d9aa9128b03b379e2fbfca14c3dabd720b1073b
anirudhkrishna22 <anirudhkrishna22@gmail.com>
2025-06-10 21:40:11 +0530

    Initial commit: Chat app project setup with spring boot web socket and config files

---
+++ src/main/java/com/example/webSocket_app/WebSocketAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.webSocket_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketAppApplication.class, args);
+	}
+
+}
